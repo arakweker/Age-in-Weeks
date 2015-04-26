03b09820469ff32d8119698192a45836580b01bd
arakweker <janvanarkel@me.com>
2015-04-26 23:31:11 +0200

    Added icons.

--- Age in Weeks/ViewController.swift
+++ Age in Weeks/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ageLabel: UILabel!
+    @IBOutlet weak var dateSelection: UIDatePicker!
+    @IBOutlet weak var componentLabel: UILabel!
+    @IBOutlet weak var nrOfDays: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func myCalculatieButtonPressed(sender: AnyObject) {
+        
+        var dateFormatter = NSDateFormatter()
+        dateFormatter.dateStyle = NSDateFormatterStyle.FullStyle
+        var strDate = dateFormatter.stringFromDate(dateSelection.date)
+        ageLabel.text = "Birthdate: " + strDate
+        
+        var calendar = NSCalendar(identifier: NSCalendarIdentifierGregorian)
+        var toDay = NSDate()
+        var durationDateComponets = calendar!.components(NSCalendarUnit.CalendarUnitDay,
+            fromDate: dateSelection.date,
+            toDate: toDay,
+            options: nil)
+        
+        var aantalDagenInLeven = durationDateComponets.day
+        var aantalWekenInLeven = Int(aantalDagenInLeven / 7)
+        var restDagen = aantalDagenInLeven - (aantalWekenInLeven * 7)
+        
+        componentLabel.text = "Alive for \(aantalWekenInLeven) weeks and \(restDagen) days"
+        nrOfDays.text = "Alive for \(aantalDagenInLeven) days"
+        ageLabel.hidden = false
+        componentLabel.hidden = false
+        nrOfDays.hidden = false
+    }
 }
 
